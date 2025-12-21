1c5d916c4ae7d3b876a3195c7df1e0713a048e2f
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-12-21 17:41:07 -0300

    criando as views e urls de autor

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("repo/admin/", admin.site.urls),
+    path("repo/autor/", include("apps.autor.urls", namespace="autor")),
 ]
