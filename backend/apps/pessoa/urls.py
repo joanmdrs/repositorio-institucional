d95b7017bbdfddca217761fe12b534fa14898dcf
Joan Medeiros <joanmedeiros2018@gmail.com>
2026-01-02 14:11:38 -0300

    migrando o código de app pessoa para usar ViewSet

--- backend/apps/pessoa/urls.py
+++ backend/apps/pessoa/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from .views import CriarPessoaView, AtualizarPessoaView, ListarPessoasView, ObterPessoaPeloIdView, ExcluirPessoaView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PessoaViewSet
+
+router = DefaultRouter()
+router.register(r'', PessoaViewSet, basename='pessoa')
 
 app_name = 'pessoa'
 
 urlpatterns = [
-    path('criar/', CriarPessoaView.as_view(), name='criar_pessoa'),
-    path('atualizar/<int:pessoa_id>/', AtualizarPessoaView.as_view(), name='atualizar_pessoa'),
-    path('listar/', ListarPessoasView.as_view(), name='listar_pessoas'),
-    path('obter-pelo-id/<int:pessoa_id>/', ObterPessoaPeloIdView.as_view(), name='obter_pessoa_pelo_id'),
-    path('excluir/<int:pessoa_id>/', ExcluirPessoaView.as_view(), name='excluir_pessoa_pelo_id')
+    path('', include(router.urls)),
 ]
--- backend/apps/pessoa/views.py
+++ backend/apps/pessoa/views.py
@@ -1,76 +1,24 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import PessoaReadSerializer, PessoaWriteSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework.filters import SearchFilter
+
 from .models import Pessoa
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import PessoaReadSerializer, PessoaWriteSerializer
+from core.pagination import DefaultPagination
 
-class CriarPessoaView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request):
-        try:
-            pessoa = PessoaWriteSerializer(data=request.data, many=False)
-            if pessoa.is_valid():
-                pessoa.save()
-                return Response({"message": "Pessoa criada com sucesso!"}, status=status.HTTP_200_OK)
-            
-            return Response(pessoa.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class AtualizarPessoaView(APIView):
+class PessoaViewSet(ModelViewSet):
+    queryset = Pessoa.objects.all().order_by('id')
     permission_classes = [AllowAny]
-    def patch(self, request, pessoa_id):
-        try:
-            pessoa = Pessoa.objects.get(id=pessoa_id)
-            print(request.data)
-            
-            serializer = PessoaWriteSerializer(pessoa, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({"message": "Pessoa atualizada com sucesso!"}, status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-        except Pessoa.DoesNotExist:
-            return Response({"error": "Pessoa não encontrada"}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-class ListarPessoasView(APIView):
-    permission_classes = [AllowAny]
-    def get(self, request):
-        try:
-            pessoas = Pessoa.objects.all()
-            serializer = PessoaReadSerializer(pessoas, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-class ObterPessoaPeloIdView(APIView):
-    permission_classes = [AllowAny]
-    def get(self, request, pessoa_id):
-        try:
-            pessoa = Pessoa.objects.get(id=pessoa_id)
-            serializer = PessoaReadSerializer(pessoa, many=False)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        except Pessoa.DoesNotExist:
-            return Response({"error": "Pessoa não encontrada"}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-class ExcluirPessoaView(APIView):
-    permission_classes = [AllowAny]
-    def delete(self, request, pessoa_id):
-        try:
-            pessoa = Pessoa.objects.get(id=pessoa_id)
-            pessoa.delete()    
-            return Response({"message": "Pessoa excluída com sucesso!"}, status=status.HTTP_200_OK)
-        
-        except Pessoa.DoesNotExist:
-            return Response({"error": "Pessoa não encontrada"}, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    pagination_class = DefaultPagination
+
+    filter_backends = [SearchFilter]
+    search_fields = [
+        'nome',
+        'cpf'
+    ]
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return PessoaReadSerializer
+        return PessoaWriteSerializer
